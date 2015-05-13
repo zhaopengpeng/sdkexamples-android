4192d305d46a992b2713f2c23cdb51a3716fa63e
zhanw <258653667@qq.com>
2015-05-13 18:24:48 +0800

    fix contact list sort wrongbug

--- ChatDemoUI/src/com/easemob/chatuidemo/activity/ContactlistFragment.java
+++ ChatDemoUI/src/com/easemob/chatuidemo/activity/ContactlistFragment.java
@@ -345,13 +345,11 @@ public class ContactlistFragment extends Fragment {
 			}
 		});
 
-		// 加入"申请与通知"和"群聊"
-		if(users.get(Constant.GROUP_USERNAME) != null)
-		    contactList.add(0, users.get(Constant.GROUP_USERNAME));
-		
-		// 加入"申请与通知"和"群聊"
+		// 加入"群聊"和"聊天室"
         if(users.get(Constant.CHAT_ROOM) != null)
             contactList.add(0, users.get(Constant.CHAT_ROOM));
+        if(users.get(Constant.GROUP_USERNAME) != null)
+            contactList.add(0, users.get(Constant.GROUP_USERNAME));
         
 		// 把"申请与通知"添加到首位
 		if(users.get(Constant.NEW_FRIENDS_USERNAME) != null)
