b0b3ed7cfef77d138a124af65e8bb6dbe284187b
youni <syyorient@gmail.com>
2015-05-13 18:25:26 +0800

    register new offlinemsg event

--- ChatDemoUI/src/com/easemob/chatuidemo/activity/ChatActivity.java
+++ ChatDemoUI/src/com/easemob/chatuidemo/activity/ChatActivity.java
@@ -1343,7 +1343,7 @@ public class ChatActivity extends BaseActivity implements OnClickListener, EMEve
 		// register the event listener when enter the foreground
 		EMChatManager.getInstance().registerEventListener(
 				this,
-				new EMNotifierEvent.Event[] { EMNotifierEvent.Event.EventNewMessage,
+				new EMNotifierEvent.Event[] { EMNotifierEvent.Event.EventNewMessage,EMNotifierEvent.Event.EventOfflineMessage,
 						EMNotifierEvent.Event.EventDeliveryAck, EMNotifierEvent.Event.EventReadAck });
 	}
 
